18bfcfb8fcd500736b3ad9b8ffa9171bc903af12
calderonsamuel <samuel.calderon@uarm.pe>
2022-10-07 21:58:27 -0500

    stop using htmlwidgets

--- R/bscard.R
+++ R/bscard.R
@@ -1,73 +1,22 @@
-#' Bootstrap Card
+#' Second iteration of bscard
 #'
-#' Card compatible with BS version 5. Imitates the behavior of the Bs4Dash::box.
+#' @param elementId id 
 #'
-#' @import htmlwidgets
-#' @importFrom htmltools tags
-#' @importFrom bslib card card_header card_body
-#'
+#' @return shiny.tag
 #' @export
-bscard <-function(width = NULL, height = NULL, elementId = NULL) {
-    
-    message <- tags$div(
-        bscard_template(elementId = elementId)
+bscard <- function(elementId = NULL) {
+    bscard_dep <- htmltools::htmlDependency(
+        name = "bscard", 
+        version = "0.0.0.9000", 
+        src = system.file(package = "bsCard"), 
+        script = "bscard.js"
+        
     )
     
-    # message <- tags$div(tags$p("hi", style = "color: red"))
-    
-    
-    # forward options using x
-    x = list(message = as.character(message))
-
-    # create widget
-    htmlwidgets::createWidget(
-        name = 'bscard',
-        x,
-        width = width,
-        height = height,
-        package = 'bsCard',
-        elementId = elementId
-        # preRenderHook = bsCardHook
+    tag <- htmltools::tagList(
+        bscard_template(elementId = elementId),
+        bscard_dep
     )
-}
-
-#' Shiny bindings for bscard
-#'
-#' Output and render functions for using bscard within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a bscard
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name bscard-shiny
-#'
-#' @export
-bscardOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'bscard', width, height, package = 'bsCard')
-}
-
-#' @rdname bscard-shiny
-#' @export
-renderBscard <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, bscardOutput, env, quoted = TRUE)
-}
-
-bsCardHook <- function(instance) {
-    theme <- bslib::bs_current_theme()
-    if (!bslib::is_bs_theme(theme)) {
-        return(instance)
-    }
-    instance$x$theme <- modifyList(
-        instance$x$theme, as.list(
-            bslib::bs_get_variables(theme, c("bg", "fg"))
-        )
-    )
-    instance
+    
+    htmltools::browsable(tag)
 }
