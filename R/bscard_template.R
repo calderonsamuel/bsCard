a50fac851e4fad0b8088d4433d43b0254f554987
calderonsamuel <samuel.calderon@uarm.pe>
2022-10-13 20:25:45 -0500

    use bsicons instead of shiny

--- R/bscard_template.R
+++ R/bscard_template.R
@@ -1,10 +1,13 @@
 #' Template for bscard
 #'
 #' @return shiny.tag
+#' @importFrom bslib card card_header card_body
+#' @importFrom bsicons bs_icon
+#' @import htmltools
 #' @export
 #'
 bscard_template <- function(width = NULL, height = NULL, elementId = NULL) {
-    bslib::card(
+    card(
         card_header(
             tags$a(
                 class = "btn card-header-toggle",
@@ -13,18 +16,18 @@ bscard_template <- function(width = NULL, height = NULL, elementId = NULL) {
                 tags$div(
                     style = "display: grid; grid-template-columns: auto auto; gap: 5px;",
                     tags$div(tags$p("Descripción de tarea", style = "margin-bottom: 0rem; text-align: left;")),
-                    tags$div(tags$p(shiny::icon("bars"), style = "margin-bottom: 0rem; text-align: right;"))
+                    tags$div(tags$p(bs_icon("chevron-expand"), style = "margin-bottom: 0rem; text-align: right;"))
                 ),
             )
         ),
-        bslib::card_body(
+        card_body(
             gap = 0,
-            class = "card-bodyid",
+            class = "card-body-for-toggle",
             style = "display: none !important;",
-            inline_description("user", "Nombre de encargado"),
-            inline_description("book-reader", "Nombre de responsable"),
-            inline_description("calendar-day", "31/12/2022"),
-            inline_description("project-diagram", "Proceso al que pertenece")
+            inline_description("person", "Nombre de encargado"),
+            inline_description("stoplights", "Nombre de responsable"),
+            inline_description("calendar4-event", "31/12/2022"),
+            inline_description("diagram-2", "Proceso al que pertenece")
         ),
     )
 }
@@ -33,7 +36,7 @@ bscard_template <- function(width = NULL, height = NULL, elementId = NULL) {
 inline_description <- function(icon_name, label) {
     tags$div(
         style = "display: grid; grid-template-columns: 20px auto; gap: 5px; width: 100%;",
-        tags$div(tags$p(shiny::icon(icon_name), style = "margin-bottom: 0rem; text-align: left;")),
+        tags$div(tags$p(bs_icon(icon_name, size = "1.1rem"), style = "margin-bottom: 0rem; text-align: left;")),
         tags$div(tags$p(label, style = "margin-bottom: 0rem; text-align: left;"))
     )|> suppressMessages()
 }
