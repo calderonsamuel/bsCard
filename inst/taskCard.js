1d5458a0207482b2e8c9252ec40885b88c24d3e8
Samuel Calderon <samuel.calderon@uarm.pe>
2022-10-17 16:07:56 -0500

    set input value for id of task_for_deleting

--- inst/taskCard.js
+++ inst/taskCard.js
@@ -1,3 +1,4 @@
+/* Functions for toggle of the tas-card-body */
 let toggleCardBody = (el) => {
     
     let cardHeader = el
@@ -31,10 +32,33 @@ function cardBodyGlobalListener() {
     }
 }
 
+
+
+/* Functions for setting an input value for task deleting */
+
+function setTaskForDeleting() {
+    let btns = document.getElementsByClassName('task-card-btn-delete')
+    const regex = /[0-9a-f]{32}/ 
+    
+    for (const btn of btns) {
+        btn.addEventListener('click', (event) => {
+            Shiny.setInputValue("task_card_to_delete", btn.id.match(regex) , {priority: "event"});
+        })
+    }
+}
+
+
+
+
+
+/* Setting all event listeners */
+
 document.addEventListener('shiny:connected', function(event) {
     cardBodyGlobalListener()
+    setTaskForDeleting()
 })
 
 window.addEventListener('load', function(event) {
     cardBodyGlobalListener()
+    setTaskForDeleting()
 })
