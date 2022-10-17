c75de9d07e251d86f98fcc69eca950e65d11cb01
Samuel Calderon <samuel.calderon@uarm.pe>
2022-10-17 11:40:25 -0500

    fix #1

--- R/bscard_template.R
+++ R/bscard_template.R
@@ -22,61 +22,39 @@ task_card_template <- function(elementId = NULL,
     date_due <- check_task_date(date_due)
     process <- check_process(process)
     
-    custom_card_style <- css(
-        `background-color` = bg
-    )
-    
-    custom_header_style <- css(
-        `background-color`= "rgb(255 255 255 / 0%)"
-    )
-    
-    custom_body_style <- css(
-        display = "none !important",
-        `background-color`= "rgb(255 255 255 / 33%)"
-    )
-    
     card(
         id = card_id,
         `data-rank-id` = card_id,
         class = paste0("task-bg-", bg),
-        # style = custom_card_style,
         card_header(
-            style = custom_header_style,
+            class = "task-card-header",
             tags$a(
                 class = "btn card-header-toggle",
-                style = "width:100%; padding: 0;",
                 id = paste0(card_id, "-header"),
                 tags$div(
-                    style = "display: grid; grid-template-columns: auto auto; gap: 5px;",
-                    tags$div(tags$p(task_description, style = "margin-bottom: 0rem; text-align: left;")),
-                    tags$div(tags$p(bs_icon("chevron-expand"), style = "margin-bottom: 0rem; text-align: right;"))
+                    class = "task-card-header-text",
+                    tags$div(tags$p(task_description, class = "task-card-header-description")),
+                    tags$div(tags$p(bs_icon("chevron-expand"), class = "task-card-header-icon"))
                 ),
             )
         ),
         card_body(
-            class = "card-body-for-toggle",
-            style = custom_body_style,
+            class = "task-card-body py-2",
             inline_description("person", assignee),
             inline_description("stoplights", reviewer),
             inline_description("calendar4-event", date_due),
             inline_description("diagram-2", process),
-            actionButton(paste0(card_id, "_delete"), "Eliminar")
+            actionButton(paste0(card_id, "_delete"), "Eliminar", class = "btn-sm mt-2 task-card-btn-delete")
         ),
     )
 }
 
 
 inline_description <- function(icon_name, label) {
-    custom_css_div <- css(
-        display = "grid",
-        `grid-template-columns` = "20px auto",
-        gap = "5px",
-        width = "100%"
-    )
     tags$div(
-        style = "display: grid; grid-template-columns: 20px auto; gap: 5px; width: 100%;",
-        tags$div(tags$p(bs_icon(icon_name, size = "1.1rem"), style = "margin-bottom: 0rem; text-align: left;")),
-        tags$div(tags$p(label, style = "margin-bottom: 0rem; text-align: left;"))
+        class = "task-card-body-inline",
+        tags$div(tags$p(bs_icon(icon_name, size = "1.1rem"), class = "task-card-body-inline-icon")),
+        tags$div(tags$p(label, class = "task-card-body-inline-label"))
     )
 }
 
